6699fdc0050987bc7504ea347ff872a446488e9f
cmata-sbu <christopher.mata@stonybrook.edu>
2025-11-14 23:38:02 -0500

    Finish commit for Expression Eval

--- src/hw7.c
+++ src/hw7.c
@@ -8,11 +8,17 @@ int precedence(char c) {
         default: return -1;
     }
 }
-inline void push(char* op_stack, int* top, char c) {
+inline void push_cp(char* op_stack, int* top, char c) {
     op_stack[++(*top)] = c;
 }
-inline char pop(char* op_stack, int* top) {
+inline char pop_cp(char* op_stack, int* top) {
     return op_stack[(*top)--];
+}
+inline void push_mp(matrix_sf** mat_stack, int* top, matrix_sf* mat) {
+    mat_stack[++(*top)] = mat;
+}
+inline matrix_sf* pop_mp(matrix_sf** mat_stack, int* top) {
+    return mat_stack[(*top)--];
 }
 // END My Helper Functions
 
@@ -66,30 +72,30 @@ char* infix2postfix_sf(char *infix) {
 
         // If the character is an operand or the transpose operator, append it to the result
         if ((c >= 'A' && c <= 'Z') || c == '\'') result[pos++] = c;
-        // If the character is '(' push it to the stack
-        else if (c == '(') push(op_stack, &top, c);
+        // If the character is '(' push_cp it to the stack
+        else if (c == '(') push_cp(op_stack, &top, c);
         else if(c == ')') {
             // dump the stack onto result
             while (top != -1 && op_stack[top] != '(') {
-                result[pos++] = pop(op_stack, &top);
+                result[pos++] = pop_cp(op_stack, &top);
             }
             
-            // pop the opening parenthesis
-            (void)pop(op_stack, &top);
+            // pop_cp_cp the opening parenthesis
+            (void)pop_cp(op_stack, &top);
         }
         else { // This is an operator which is not '(' or ')'
             while (top != -1 && op_stack[top] != '(' && (precedence(op_stack[top]) > precedence(c) || (precedence(op_stack[top]) == precedence(c)))) {
-                result[pos++] = pop(op_stack, &top);
+                result[pos++] = pop_cp(op_stack, &top);
             }
-            push(op_stack, &top, c);
+            push_cp(op_stack, &top, c);
         }
 
         infix_ro++;
     }
 
-    // Pop any remaining operators
+    // pop_cp any remaining operators
     while (top != -1) {
-        result[pos++] = pop(op_stack, &top);
+        result[pos++] = pop_cp(op_stack, &top);
     }
 
     result[pos] = '\0';
@@ -97,7 +103,60 @@ char* infix2postfix_sf(char *infix) {
 }
 
 matrix_sf* evaluate_expr_sf(char name, char *expr, bst_sf *root) {
-    return NULL;
+    char* postfix_expr = infix2postfix_sf(expr);
+    if (postfix_expr == NULL) return NULL;
+
+    char* postfix_expr_ro = postfix_expr;
+    matrix_sf* matrix_stack[strlen(postfix_expr_ro)];
+    int top = -1;
+
+    while(*postfix_expr_ro != '\0') {
+        char c = *postfix_expr_ro;
+
+        if (c >= 'A' && c <= 'Z') {
+            matrix_sf* M = find_bst_sf(c, root);
+            push_mp(matrix_stack, &top, M);
+        }
+        else if(c == '\'') {
+            matrix_sf *M = pop_mp(matrix_stack, &top);
+            matrix_sf* T = transpose_mat_sf(M);
+
+            T->name = '@';
+            push_mp(matrix_stack, &top, T);
+
+            if (M->name == '@') free(M);
+        }
+        else if(c == '+') {
+            matrix_sf* right = pop_mp(matrix_stack, &top);
+            matrix_sf* left = pop_mp(matrix_stack, &top);
+            
+            matrix_sf* M = add_mats_sf(left, right);
+            M->name == '@';
+            push_mp(matrix_stack, &top, M);
+
+            if (right->name == '@') free(right);
+            if (left->name == '@') free(left);
+        }
+        else if(c == '*') {
+            matrix_sf* right = pop_mp(matrix_stack, &top);
+            matrix_sf* left = pop_mp(matrix_stack, &top);
+
+            matrix_sf* M = mult_mats_sf(left, right);
+            M->name = '@';
+            push_mp(matrix_stack, &top, M);
+
+            if (right->name == '@') free(right);
+            if (left->name == '@') free(left);
+        }
+
+        postfix_expr_ro++;
+    }
+
+    matrix_sf* final = pop_mp(matrix_stack, &top);
+    final->name = name;
+
+    free(postfix_expr);
+    return final;
 }
 
 matrix_sf *execute_script_sf(char *filename) {
